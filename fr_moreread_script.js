91e1c911e3a17aa33a28722ac4c7d1d302463ec9
Gunji Norihito <gunji11ganji@gmail.com>
2015-09-30 03:23:58 +0900

    フロントページへにもっと読むを追記

---
+++ fr_moreread_script.js
@@ -0,0 +1,9 @@
+
+$function(){
+	'use strict';
+   	 $('#main :nth-child(4)');
+        	 $('#fr_moreread_button').on('click', function() {
+           	 $('#fr_moreread_button').fadeOut('fast');
+           	 $('#recent :nth-child(n+5)').fadeIn(1700);
+        });
+    });
