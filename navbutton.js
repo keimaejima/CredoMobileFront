6682c6ad058b3abee589b36cbb33fdab2b38163c
fufufukakaka <demonstrundum@gmail.com>
2015-10-04 14:41:37 +0900

    プルリク#18でのコメントを反映しました！

--- navbutton.js
+++ navbutton.js
@@ -1,19 +1,8 @@
 //.buttonがクリックされたら、(1).buttonに.actionを追加 / (2).navigation-contentsに.openを追加 / (3) sectionに.closeを追加する
 $(function () {
-    $('.button').click(function () {
+    $('.hb-navigation .button').click(function () {
         $(this).toggleClass('action');
         $('.navigation-contents').toggleClass('open');
         $('segment').toggleClass('close');
     });
-});
-
-
-
-//windowの高さを取得する
-var WindowHeight = $(window).height();
-$(function () {
-    if (WindowHeight > 0) {
-        //class="section"の要素に高さを書き込む
-        $('.segment').css('height', WindowHeight + 'px');
-    }
 });
