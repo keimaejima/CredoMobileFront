e4a42238bb05bed25747174a36c0552d44e177a4
Gunji Norihito <gunji11ganji@gmail.com>
2015-10-04 21:53:55 +0900

    もっと読むボタンのプルリクの修正を行いました。

--- fr_moreread_script.js
+++ fr_moreread_script.js
@@ -1,9 +1,7 @@
 
-$function(){
-	'use strict';
-   	 $('#main :nth-child(4)');
-        	 $('#fr_moreread_button').on('click', function() {
-           	 $('#fr_moreread_button').fadeOut('fast');
-           	 $('#recent :nth-child(n+5)').fadeIn(1700);
+$(function(){
+        $('#fr_moreread_button').on('click', function() {
+            $('#fr_moreread_button').fadeOut('fast');
+            $('#recent :nth-child(n+5)').fadeIn(1700);
         });
     });
