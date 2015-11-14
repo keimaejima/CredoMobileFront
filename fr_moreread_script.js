da36649d6f13238097a5d89ab58afbbbaa4a5a98
takashi <tomorrowgaarusa@gmail.com>
2015-11-14 09:35:50 +0900

    replace $ to jQuery

--- fr_moreread_script.js
+++ fr_moreread_script.js
@@ -1,7 +1,7 @@
 
-$(function(){
-        $('#recent .fr_btn').on('click', function() {
-            $('#recent .fr_btn').fadeOut('fast');
-            $('#recent :nth-child(n+5)').fadeIn(1700);
+jQuery(function(){
+        jQuery('#recent .fr_btn').on('click', function() {
+            jQuery('#recent .fr_btn').fadeOut('fast');
+            jQuery('#recent :nth-child(n+5)').fadeIn(1700);
         });
     });
